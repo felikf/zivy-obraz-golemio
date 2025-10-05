231c26b9dcdb37a7b10b129f4defde0776b0c501
Felix Fatka <FELIXFA@amdocs.com>
2025-10-05 08:26:13 +0200

    feat: sync using workflow

--- src/traffic-sync.mjs
+++ src/traffic-sync.mjs
@@ -1,4 +1,4 @@
-import { filter, tap, timer } from 'rxjs';
+import { filter, tap, timer, of } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 
 import { fetchDepartureData } from './utils/download-golemio.mjs';
@@ -6,15 +6,22 @@ import { formatDate } from './utils/util.mjs';
 import { uploadData } from './utils/upload.mjs';
 import { JESENICE_PLATFORM_B } from './utils/constants.mjs';
 
-const stopId = process.argv[2] || JESENICE_PLATFORM_B;
+const intervalTime = parseInt(process.argv[2]) || 5;
+const stopId = process.argv[3] || JESENICE_PLATFORM_B;
+const intervalMs = intervalTime * 60 * 1000;
 
-console.log(`Fetching data: ${formatDate(new Date())} for stop ${stopId}.`);
+console.log(`Starting: timetable fetch interval ${intervalMs / 1000 / 60}m`);
 
-// fetchDepartureData(stopId, -9, stopId).then(queryTimeTable => {
-//   if (queryTimeTable.length > 0) {
-//   uploadData(queryTimeTable)
-// }
-// console.log('Timetable successfully posted.')
-// }).catchError(err => {
-//   console.error('Error fetching or uploading data:', err);
-// });
+// Set up an interval to fetch data every n minute
+// timer(0, intervalMs)
+of(0)
+  .pipe(
+    tap(() => console.log(`Fetching data: ${formatDate(new Date())}`)),
+    switchMap(() => fetchDepartureData(stopId, -9)),
+    filter(queryTimeTable => queryTimeTable.length > 0),
+    switchMap(queryTimeTable => uploadData(queryTimeTable))
+  )
+  .subscribe({
+    next: () => console.log('Timetable successfully posted.'),
+    error: error => console.error('Error occurred:', error)
+  });
