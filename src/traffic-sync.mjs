3588059c01be00a1b64d0774bb285ef81065a585
Felix Fatka <FELIXFA@amdocs.com>
2025-10-05 08:18:27 +0200

    feat: sync using workflow

--- src/traffic-sync.mjs
+++ src/traffic-sync.mjs
@@ -7,18 +7,14 @@ import { uploadData } from './utils/upload.mjs';
 import { JESENICE_PLATFORM_B } from './utils/constants.mjs';
 
 const stopId = process.argv[2] || JESENICE_PLATFORM_B;
-const intervalTime = parseInt(process.argv[3]) || 5;
-const intervalMs = intervalTime * 60 * 1000;
 
-console.log(`Fetching data: ${formatDate(new Date())}`);
+console.log(`Fetching data: ${formatDate(new Date())} for stop ${stopId}.`);
 
-fetchDepartureData(stopId, -9, stopId).then(queryTimeTable => {
-  if (queryTimeTable.length > 0) {
-  uploadData(queryTimeTable)
-}
-console.log('Timetable successfully posted.')
-}).catchError(err => {
-  console.error('Error fetching or uploading data:', err);
-});
-
-
+// fetchDepartureData(stopId, -9, stopId).then(queryTimeTable => {
+//   if (queryTimeTable.length > 0) {
+//   uploadData(queryTimeTable)
+// }
+// console.log('Timetable successfully posted.')
+// }).catchError(err => {
+//   console.error('Error fetching or uploading data:', err);
+// });
