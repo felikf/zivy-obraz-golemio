a9545ae725c02047a5369559c9355170be5ece21
felix <felix.fatka@gmail.com>
2025-10-06 09:09:44 +0200

    fix: proverb sync

--- src/proverb-sync.mjs
+++ src/proverb-sync.mjs
@@ -1,17 +1,15 @@
-import { timer } from 'rxjs';
+import { of } from 'rxjs';
 import { map, switchMap } from 'rxjs/operators';
 import { getRandomProverb } from './utils/proverb.mjs';
 import { uploadData } from './utils/upload.mjs';
 
-const intervalTime = parseInt(process.argv[2]) || 5;
-const wrapLength = parseInt(process.argv[3]) || 0;
-const intervalMs = intervalTime * 60 * 1000;
+const wrapLength = parseInt(process.argv[2]) || 0;
 
-let interval = (10 * intervalMs) / 1000 / 60;
-console.log(`Starting: proverb fetch interval ${interval}m, wrap length: ${wrapLength}`);
+console.log(`Starting: proverb sync, wrap length: ${wrapLength}`);
 
 // Set up an interval to fetch data every n minute
-timer(0, intervalMs)
+// timer(0, intervalMs)
+of(true)
   .pipe(
     map(() => getRandomProverb(wrapLength)),
     map(
@@ -20,6 +18,6 @@ timer(0, intervalMs)
     switchMap(queryString => uploadData(queryString))
   )
   .subscribe({
-    next: () => console.log('Proverb successfully posted.'),
+    next: queryString => console.log(`Proverb successfully posted: ${queryString}`),
     error: error => console.error('Error occurred:', error)
   });
