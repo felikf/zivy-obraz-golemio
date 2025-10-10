f6c61c0fb81246dfc6b0ada17227dcac63a0c5e0
felix <felix.fatka@gmail.com>
2025-10-10 21:44:17 +0200

    fix: timestamp

--- src/utils/util.mjs
+++ src/utils/util.mjs
@@ -1,18 +1,15 @@
-export function formatDate(date) {
-  const hours = String(date.getHours()).padStart(2, '0');
-  const minutes = String(date.getMinutes()).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const year = date.getFullYear();
-
-  return `${hours}:${minutes} ${day}.${month}.${year}`;
-}
-
-export function formatTime(timeStamp) {
+export function formatDate(timeStamp) {
   return new Intl.DateTimeFormat('cs-CZ', {
     dateStyle: 'short',
     timeStyle: 'short'
-  }).format(new Date(timeStamp));
+  }).format(timeStamp);
+}
+
+export function formatTime(timeStamp) {
+  return new Date(timeStamp).toLocaleTimeString('cs-CZ', {
+    hour: '2-digit',
+    minute: '2-digit'
+  });
 }
 
 export function getGolemioToken() {
